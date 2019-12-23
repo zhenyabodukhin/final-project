09b8342f94c06f0cbf19091eadb6b6d978c2c486
Viachaslau Kalevich <cto@noirix.com>
2019-12-23 13:00:19 +0300

    Initial commit

---
+++ src/main/java/com/htp/Car.java
@@ -0,0 +1,17 @@
+package com.htp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Car {
+    private Long id;
+    private String carName;
+}
+
+
---
+++ src/main/java/com/htp/TestClass.java
@@ -0,0 +1,7 @@
+package com.htp;
+
+public class TestClass {
+    public static void main(String[] args) {
+        System.out.println(new Car().getId());
+    }
+}
