161423d32c412482b910c44025d039329090ac8d
zhenyabodukhin <zhenyabodukhin@mail.ru>
2020-03-15 20:11:44 +0300

    Spring Data service changes #2

--- src/main/java/com/htp/service/UserService.java
+++ src/main/java/com/htp/service/UserService.java
@@ -1,4 +1,18 @@
 package com.htp.service;
 
+import com.htp.domain.User;
+
+import java.util.List;
+
 public interface UserService {
+
+    List<User> findAll();
+
+    User save (User user);
+
+    User update (User user);
+
+    void delete (Long id);
+
+    User findById(Long id);
 }
