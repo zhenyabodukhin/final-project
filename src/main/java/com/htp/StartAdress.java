16c948e708a97b2fd6bd5c0262d173cbb1c332e6
zhenyabodukhin <zhenyabodukhin@mail.ru>
2020-03-16 17:14:46 +0300

    Spring Data changes №4

--- src/main/java/com/htp/StartAdress.java
+++ src/main/java/com/htp/StartAdress.java
@@ -14,32 +14,28 @@ public class StartAdress {
 
         AdressServiceImpl adressServiceImpl = (AdressServiceImpl) context.getBean("AdressServiceImpl");
 
-        System.out.println("Метод Adress findAll");
-        for(Adress adress : adressServiceImpl.findAll()){
-            System.out.println(adress.toString());
-        }
+//        System.out.println("Метод Adress findAll");
+//        for(Adress adress : adressServiceImpl.findAll()){         //OK
+//            System.out.println(adress.toString());
+//        }
 
-        System.out.println("Метод Adress findContainsValue");
-        for(Adress adress : adressServiceImpl.findContainsValue("Mi")){
-            System.out.println(adress.toString());
-        }
-
-        System.out.println("Метод Adress save");
-        Adress adress = new Adress("Mickevich", "39", 1, 2,
-                2, false);
-        Adress adress1 = new Adress("Mickevi", "30", 10, 2,
-                2, false);
-        System.out.println(adressServiceImpl.findAll());
-        System.out.println(adressServiceImpl.save(adress1));
-
-        System.out.println("Метод Adress update");
-        Adress secondAdress = new Adress(1L,"Mickevicha", "30", 10, 2,
-                2, false);
-        System.out.println(adressServiceImpl.update(secondAdress));
-
-        System.out.println("Метод Adress delete");
-        adressServiceImpl.delete(2L);
-
+//        System.out.println("Метод Adress findContainsValue");
+//        for(Adress adress : adressServiceImpl.findContainsValue("Mi")){
+//            System.out.println(adress.toString());
+//        }
+//
+//        System.out.println("Метод Adress save");
+//        Adress adress = new Adress("Lomonosova", "43", 1, 2, 2, false);  //OK
+//        System.out.println(adressServiceImpl.save(adress));
+//
+//        System.out.println("Метод Adress update");
+//        Adress secondAdress = new Adress(1L,"Mickevicha", "30",                   //OK
+//                15, 3, 2, false);
+//        System.out.println(adressServiceImpl.update(secondAdress));
+//
+//        System.out.println("Метод Adress delete");      //OK
+//        adressServiceImpl.delete(3L);
+//
         System.out.println("Метод Adress findById");
         System.out.println(adressServiceImpl.findById(1L));
     }
