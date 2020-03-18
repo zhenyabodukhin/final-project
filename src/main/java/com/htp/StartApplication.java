c58b2456e6de272fad445ac55ee0415af3dab05c
zhenyabodukhin <zhenyabodukhin@mail.ru>
2020-03-18 19:56:42 +0300

    app starter

---
+++ src/main/java/com/htp/StartApplication.java
@@ -0,0 +1,70 @@
+package com.htp;
+
+import com.htp.config.core.DatabaseConfig;
+import com.htp.config.core.JpaConfig;
+import com.htp.config.swagger.SwaggerConfig;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.Import;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+@EnableSwagger2
+@EnableAspectJAutoProxy
+@EnableTransactionManagement(proxyTargetClass = true)
+@SpringBootApplication(scanBasePackages = {"com.htp"},
+        exclude = {
+                JacksonAutoConfiguration.class,
+                HibernateJpaAutoConfiguration.class
+        })
+@Import({
+        DatabaseConfig.class,
+        JpaConfig.class,
+        SwaggerConfig.class
+})
+public class StartApplication extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(StartApplication.class, args);
+    }
+
+    @Autowired
+    @Bean(name = "sessionFactory")
+    public SessionFactory getSessionFactory(DataSource dataSource) throws Exception {
+        // Fix Postgres JPA Error:
+        // Method org.postgresql.jdbc.PgConnection.createClob() is not yet implemented.
+        // properties.put("hibernate.temp.use_jdbc_metadata_defaults",false);
+
+        LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
+
+        // Package contain entity classes
+        factoryBean.setPackagesToScan("com.htp");
+        factoryBean.setDataSource(dataSource);
+        factoryBean.setHibernateProperties(getAdditionalProperties());
+        factoryBean.afterPropertiesSet();
+        //
+        SessionFactory sf = factoryBean.getObject();
+        System.out.println("## getSessionFactory: " + sf);
+        return sf;
+    }
+
+
+    private Properties getAdditionalProperties() {
+        Properties properties = new Properties();
+
+        // See: application.properties
+        properties.put("hibernate.show_sql", "true");
+        properties.put("current_session_context_class", "org.springframework.orm.hibernate5.SpringSessionContext");
+        return properties;
+    }
+
+}
