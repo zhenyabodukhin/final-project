54eedc706e9569a1159b1118f463338a7803688b
zhenyabodukhin <zhenyabodukhin@mail.ru>
2020-04-10 16:43:53 +0300

    testing

--- src/main/java/com/htp/controller/exception/handler/DefaultExceptionHandler.java
+++ src/main/java/com/htp/controller/exception/handler/DefaultExceptionHandler.java
@@ -3,6 +3,10 @@ package com.htp.controller.exception.handler;
 
 import com.htp.controller.messages.ErrorMessage;
 import com.htp.exception.EntityNotFoundException;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.UnsupportedJwtException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,14 +27,27 @@ public class DefaultExceptionHandler {
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<ErrorMessage> handleNoSuchEntityException(EntityNotFoundException e){
+    public ResponseEntity<ErrorMessage> handleNoSuchEntityException(EntityNotFoundException e) {
         log.error(e.getMessage(), e);
         return new ResponseEntity<>(new ErrorMessage(e.getMessage()),
                 HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(AuthenticationException.class)
-    public ResponseEntity<ErrorMessage> handleAuthenticationException(AuthenticationException e) {
+    public ResponseEntity<ErrorMessage> handleAuthenticationException(EntityNotFoundException e) {
+        log.error(e.getMessage(), e);
+        return new ResponseEntity<>(new ErrorMessage(e.getMessage()),
+                HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler({
+            UnsupportedJwtException.class,
+            MalformedJwtException.class,
+            SignatureException.class,
+            ExpiredJwtException.class,
+            IllegalArgumentException.class
+    })
+    public ResponseEntity<ErrorMessage> handleTokenProcessException(AuthenticationException e) {
         log.error(e.getMessage(), e);
         return new ResponseEntity<>(new ErrorMessage(e.getMessage()), HttpStatus.UNAUTHORIZED);
     }
